0697010ff68c7d2ea9b1c212923ce4b51f8246b5
bitThirteen <mich1.eickmeyer@gmail.com>
2024-08-28 22:29:49 +0200

    implement reuse of same time slot in tracker

--- include/ds-compare/util/stattrak.h
+++ include/ds-compare/util/stattrak.h
@@ -17,8 +17,9 @@ enum DS_COMPARE ErrorMetric : int {
 };
 
 struct TimeMeasure {
-    std::chrono::steady_clock::time_point start;
-    std::chrono::steady_clock::time_point end;
+    boost::optional<std::chrono::steady_clock::time_point> start;
+    boost::optional<std::chrono::steady_clock::time_point> end;
+    bool finished = false;
 };
 
 struct DS_COMPARE StatTrak {
@@ -68,20 +69,23 @@ struct DS_COMPARE StatTrak {
         this->m_data.clear();
     }
 
-    void timer_start(const std::string& identifier)
+    void timer_start(const std::string& identifier = "")
     {
+        /* Check if there's even an entry for the currently active ds, if not, create one */
         auto it = this->m_data.find(this->m_active);
         if (it == this->m_data.end())
         {
             this->m_data[this->m_active];
         }
-
+        
         StatData& data = this->m_data.at(this->m_active);
-        data.m_times.emplace(identifier, TimeMeasure());
-        data.m_times[identifier].start = std::chrono::steady_clock::now();
+        data.m_times.emplace(identifier, std::vector<TimeMeasure>());
+
+        TimeMeasure& measure = get_first_available(data.m_times.at(identifier), true);
+        measure.start = std::chrono::steady_clock::now();
     }
 
-    void timer_end(const std::string& identifier)
+    void timer_end(const std::string& identifier = "")
     {
         auto end_measure = std::chrono::steady_clock::now();
 
@@ -93,11 +97,13 @@ struct DS_COMPARE StatTrak {
             SLog(EError, "Identifier '%s' could not be found.", identifier.c_str());
         }
 
-        it->second.end = end_measure;
+        TimeMeasure& measure = get_first_available(it->second, false);
+        measure.end = end_measure;
+        measure.finished = true;
     }
 
     /// Obtain the duration between two measured points specified by the identifier for the currently assigned data structure.
-    Float get_duration(const std::string& identifier)
+    Float get_duration(const std::string& identifier = "", const int slot = -1)
     {
         auto it = this->m_data.find(this->m_active);
         if (it == this->m_data.end())
@@ -106,11 +112,11 @@ struct DS_COMPARE StatTrak {
         }
 
         auto& times = this->m_data.at(this->m_active).m_times;
-        return get_duration_helper(times, identifier).count();
+        return get_duration_helper(times, identifier, slot);
     }
 
     /// Obtain the duration between two measured points specified by the identifier for the specified data structure.
-    Float get_duration(const std::string& identifier, const DSType ds)
+    Float get_duration(const DSType ds, const std::string& identifier = "", const int slot = -1)
     {
         auto data_it = this->m_data.find(ds);
         if (data_it == this->m_data.end())
@@ -119,7 +125,7 @@ struct DS_COMPARE StatTrak {
         }
 
         auto& times = data_it->second.m_times;
-        return get_duration_helper(times, identifier).count();
+        return get_duration_helper(times, identifier, slot);
     }
 
     /// Obtain the error for the currently assigned data structure.
@@ -136,7 +142,7 @@ struct DS_COMPARE StatTrak {
     }
 
     /// Obtain the error for the specified data structure.
-    Float get_error(const ErrorMetric metric, const DSType ds)
+    Float get_error(const DSType ds, const ErrorMetric metric)
     {
         auto data_it = this->m_data.find(ds);
         if (data_it == this->m_data.end())
@@ -148,6 +154,7 @@ struct DS_COMPARE StatTrak {
         return get_error_helper(errors, metric);
     }
 
+    /// Write the content of the tracker to an output file.
     void write(const std::string& path)
     {
         if (this->m_data.empty()) return;
@@ -180,15 +187,15 @@ struct DS_COMPARE StatTrak {
             const auto& times = entry.second.m_times;
 
             // Errors
-            res += (errors.find(RMSE) != errors.end()) ? std::to_string(get_error(RMSE, type)) + "," : ",";
-            res += (errors.find(PSNR) != errors.end()) ? std::to_string(get_error(PSNR, type)) + "," : ",";
-            res += (errors.find(MSE) != errors.end())  ? std::to_string(get_error(MSE, type)) + ","  : ",";
-            res += (errors.find(MAE) != errors.end())  ? std::to_string(get_error(MAE, type)) + ","  : ",";
+            res += (errors.find(RMSE) != errors.end()) ? std::to_string(get_error(type, RMSE)) + "," : ",";
+            res += (errors.find(PSNR) != errors.end()) ? std::to_string(get_error(type, PSNR)) + "," : ",";
+            res += (errors.find(MSE) != errors.end())  ? std::to_string(get_error(type, MSE)) + ","  : ",";
+            res += (errors.find(MAE) != errors.end())  ? std::to_string(get_error(type, MAE)) + ","  : ",";
 
             // Time
             for (const auto& header : time_headers)
             {
-                res += (times.find(header) != times.end()) ? std::to_string(get_duration(header, type)) + "," : ",";
+                res += (times.find(header) != times.end()) ? std::to_string(get_duration(type, header)) + "," : ",";
             }
             res += "\n";
         }
@@ -201,7 +208,7 @@ struct DS_COMPARE StatTrak {
     void operator=(StatTrak const&) = delete;
 private:
     using ErrorMap = std::map<ErrorMetric, Float>;
-    using TimesMap = std::map<std::string, TimeMeasure>;
+    using TimesMap = std::map<std::string, std::vector<TimeMeasure>>;
 
     struct StatData {
         ErrorMap m_errors;
@@ -211,14 +218,46 @@ private:
     DSType m_active;
     std::map<DSType, StatData> m_data;
 
-    std::chrono::duration<Float> get_duration_helper(const TimesMap& times, const std::string& identifier)
+    TimeMeasure& get_first_available(std::vector<TimeMeasure>& slots, bool rec_start)
+    {
+        for (auto& slot : slots)
+        {
+            if (slot.finished) continue;
+            if (rec_start && slot.start) SLog(EError, "Attempting to start a timer before finishing the previous segment.");
+
+            return slot;
+        }
+
+        if (!rec_start) SLog(EError, "Attempting to stop a timer although there is no active timer running.");
+
+        slots.push_back(TimeMeasure());
+        return slots.back();
+    }
+
+    Float get_duration_helper(const TimesMap& times, const std::string& identifier, const int slot)
     {
         auto it = times.find(identifier);
         if (it == times.end())
             SLog(EError, "Identifier '%s' could not be found.", identifier.c_str());
 
-        TimeMeasure tm = it->second;
-        return std::chrono::duration_cast<std::chrono::duration<Float>>(tm.end - tm.start);
+        auto tm = it->second;
+
+        if (slot < -1 || slot >= (int) tm.size())
+            SLog(EError, "Timing slot index is out of bounds in StatTrak::get_duration");
+
+        Float total_duration = 0;
+        for (int i = 0; i < tm.size(); ++i)
+        {
+            if (slot != -1 && slot != i) continue;
+
+            auto& measure = tm.at(i);
+            if (!measure.finished) continue;
+
+            auto diff = measure.end.get() - measure.start.get();
+            total_duration += std::chrono::duration_cast<std::chrono::duration<Float>>(diff).count();
+        }
+
+        return total_duration;
     }
 
     Float get_error_helper(const ErrorMap& errors, const ErrorMetric metric)
