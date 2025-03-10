484db391c009f080cff2a0a94abc860f26e9472b
bitThirteen <mich1.eickmeyer@gmail.com>
2025-03-10 18:29:26 +0100

    improve documentation & refactor fluid settings

--- dscom.py
+++ dscom.py
@@ -1,90 +1,113 @@
+##############################################
+# Automated DS::compare testing script for all implemented data structures.
+# Stores the results as CSV file per data structure, per configuration, in a folder.
+# Lots of folders, in fact.
+#
+# How to add your own structure to this script:
+# TBA!
+#
+# Feel free to do whatever you want to this script.
+# Michael Eickmeyer, 2025 @ TU Wien.
+##############################################
+
 import subprocess
 import os
 import time
 import math
 from concurrent.futures import ProcessPoolExecutor
 
-# TODO:
-# - Split up folders so they're 100 images at most for faster processing
-# - Iterate over settings and increase each time
-# - Store results in CSV; one folder per ds, one csv for every permutation
+##############################################
+# A bunch of classes handling fluid setting parameters.
+#
+# Fluid settings allow for a parameter to increase, decrease or toggle
+# within this test script. For instance, the Range class takes an optional
+# 'func' parameter which allows the user to specify how exactly the value
+# should increase within the range.
+#
+# The Toggle class allows the user to easily toggle between two truthy/falsy values.
+#
+# All fluid settings consist of the functions...
+# • get() -> obtain the current value
+# • reset() -> set the value back to its initial state
+# • next() -> increase the value to its next state
+# • finished() -> check whether the value has reached its final state (upper bound, !current, etc.) 
+##############################################
 
 class FluidSetting:
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.default = value
+
+    def get(self):
+        return self.value
+
+    def reset(self):
+        self.value = self.default
 
 class Range(FluidSetting):
     def __init__(self, start, end, func = None):
-        self.bounds = (start, end)
-        self.value = start
+        super().__init__(start)
+        self.end = end
         
         if not func:
             func = lambda x: x + 1
         self.func = func
-
-    def get(self):
-        return self.value
     
     def next(self):
         self.value = self.func(self.value)
 
     def finished(self):
         next_value = self.func(self.value)
-        return (next_value > self.bounds[-1])
+        return (next_value > self.end)
     
-    def reset(self):
-        self.value = self.bounds[0]
-    
-class Toggleable(FluidSetting):
+class Toggle(FluidSetting):
     def __init__(self, state):
-        self.state = state
-        self.initial = state
-
-    def get(self):
-        return self.state
+        super().__init__(state)
     
     def next(self):
-        self.state = not self.state
+        self.value = not self.value
 
     def finished(self):
-        return (self.state != self.initial)
-    
-    def reset(self):
-        self.state = self.initial
+        return (self.value != self.default)
 
-###################################################
+##############################################
+# Base config.
+##############################################
 
 settings = {
     "testing": {
         # Enables multithreading for concurrent evaluation of environment maps.
         "multithreading": True,
         # Set a time limit in seconds after which the application will stop running. If set to '-1', the time limit will be ignored.
-        "time_limit": -1
+        "time_limit": -1,
+        # Max. number of envmaps per folder. Set to -1 to disable.
+        "batch_size": 100
     },
     "general": {
         # Path to folder containing the envmaps. Set to 'None' to use the default folder. If multithreading is enabled, this setting is ignored.
         "envmap_path": None,
         # Path to folder containing the results. Set to 'None' to use the default folder. If multithreading is enabled, this setting is ignored.
         "result_path": None,
-        #
+        # Number of learning samples per data structure. These samples are used to create a guiding distribution.
         "samples_learning": Range(start=64, end=65536, func=lambda x: x * 2),
-        #
+        # Number of guiding samples per data structure. These samples are used to recreate the sampled distribution from the guiding distribution.
         "samples_guiding": 65536,
-        #
+        # A blacklist specifying which data structures should be skipped in the overall test.
         "blacklist": [],
-        #
+        # Whether to normalize the envmap each data structure is 'learning' with. (Keep this true unless you know what you're doing.)
         "normalize": True
     },
     "noise": {
-        #
+        # Whether the initial envmap should be noisified.
         "envmap": False,
-        #
+        # Whether a variable amount of noise should be introduced to each learning sample.
         "samples": False
     },
     "structures": {
         "sh": {
             "bands": Range(start=1, end=10),
             "depth": Range(start=1, end=20),
-            "use_offset": Toggleable(True)
+            "use_offset": Toggle(True)
         },
         "dt": {
             "frac_loss": "none",
@@ -107,7 +130,7 @@ settings = {
         },
         "vmf": {
             "components": Range(start=1, end=32),
-            "use_ruppert": Toggleable(True)
+            "use_ruppert": Toggle(True)
         }
     }
 }
@@ -115,10 +138,11 @@ settings = {
 base_name = "./data/tests/envmaps/"
 res_name = "./data/results/"
 
-###################################################
-################# END CONFIG ######################
-###################################################
+##############################################
+# Start of code.
+##############################################
 
+batches = []
 commands = []
 progress = {}
 futures = None
@@ -196,7 +220,6 @@ def collect_args():
     Accumulates a list of args for every sub-folder / worker based on the global settings
     """
 
-    # dont forget to add sm at end!
     commands = ["--sl", "--sg", "-b", "-n", "--ne", "--ns", "--shb", "--shd", "--sho", "--dtl", "--dtf", "--dtt", "--dti", "--dtd", "-t", "--tx", "--ty", "--bt", "--btx", "--bty", "--btd", "--btt", "--vc", "--vr"]
     commands = zip(commands, build_argvals(settings))
     
@@ -212,6 +235,9 @@ def collect_args():
             args.append(value)
         
         commands.append(args)
+
+def split_folders():
+    pass
     
 def start_comparer(command):
     subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
@@ -239,3 +265,8 @@ def run():
 
 if __name__ == '__main__':
     run()
+
+# TODO:
+# - Split up folders so they're 100 images at most for faster processing
+# - Iterate over settings and increase each time
+# - Store results in CSV; one folder per ds, one csv for every permutation
